429ea8146f948bc45a59852479c27c8e52eaa3bf
antony1803 <37068513+antony1803@users.noreply.github.com>
2019-10-20 19:32:48 +0300

    feat
    userbase

--- stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
+++ stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
@@ -3,6 +3,8 @@ package by.courses.java.streamapi.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor(staticName = "of")
 public class UserBase {
@@ -10,4 +12,37 @@ public class UserBase {
     private String name;
 
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof UserBase)) 
+            return false;
+        if (this == o) 
+            return true;
+        UserBase userBase = (UserBase) o;
+        return age == userBase.age &&
+                name.equals(userBase.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "UserBase : " +
+                "name = "  + name +
+                ", age = " + age +
+                ';';
+    }
 }
+
